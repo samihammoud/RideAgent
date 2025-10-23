f91e3d37ef014adf6ff1f235bf822ab8fdc4a129
Sami Hammoud <your@email.com>
2025-10-22 19:28:12 -0700

    rideAgentTest.py

--- rideAgentTest.py
+++ rideAgentTest.py
@@ -1,6 +1,14 @@
 import requests
+import env
 
-url = "https://jsonplaceholder.typicode.com/posts/1"
+load_dotenv()
+url = os.getenv("URL")
+token = os.getenv("AUTH_TOKEN")
+
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+}
 
 response = requests.get(url)
 
